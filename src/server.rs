089d218441553e6e0785b8dbf0b5dc4f942b1ec8
Kilerd <blove694@gmail.com>
2022-02-27 13:01:39 +0800

    feat: return account snapshot api

--- src/server.rs
+++ src/server.rs
@@ -1,15 +1,22 @@
 use crate::cli::ServerOpts;
 use crate::error::ZhangResult;
 use axum::body::{boxed, Full};
+use axum::extract::Extension;
 use axum::http::{header, StatusCode, Uri};
 use axum::response::{IntoResponse, Response};
-use axum::{routing::get, Router};
+use axum::{routing::get, AddExtensionLayer, Router, Json};
 use log::info;
 use rust_embed::RustEmbed;
 use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4};
+use std::sync::Arc;
+use tokio::sync::RwLock;
+use crate::core::ledger::Ledger;
 
-async fn hello_world() -> &'static str {
-    "hello world"
+pub type LedgerState = Arc<RwLock<Ledger>>;
+
+async fn hello_world(Extension(ledger): Extension<LedgerState>) -> impl IntoResponse {
+    let x = ledger.read().await;
+    Json(x.snapshot.clone())
 }
 
 async fn serve_frontend(uri: Uri) -> impl IntoResponse {
@@ -55,9 +62,13 @@ pub fn serve(opts: ServerOpts) -> ZhangResult<()> {
     runtime.block_on(start_server(opts))
 }
 async fn start_server(opts: ServerOpts) -> ZhangResult<()> {
+    let ledger = Ledger::load(opts.file)?;
+    let ledger_data = Arc::new(RwLock::new(ledger));
+
     let app = Router::new()
         .route("/api", get(hello_world))
-        .fallback(get(serve_frontend));
+        .fallback(get(serve_frontend))
+        .layer(AddExtensionLayer::new(ledger_data));
 
     let addr = SocketAddrV4::new(Ipv4Addr::new(0, 0, 0, 0), opts.port);
     info!("zhang is listening on http://127.0.0.1:{}/", opts.port);
