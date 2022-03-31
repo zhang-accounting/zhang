661e85d8d0122d7326d8efb0e500369a984fc622
Kilerd <blove694@gmail.com>
2022-03-31 16:07:21 +0800

    feat: account line show currency snapshot summary

--- zhang-frontend/src/components/AccountLine.tsx
+++ zhang-frontend/src/components/AccountLine.tsx
@@ -1,9 +1,11 @@
 import { Flex, Text, Box } from "@chakra-ui/react";
 import { useNavigate } from "react-router";
+import Amount from "./Amount";
 
 export default function Component({ data }) {
     let navigate = useNavigate();
 
+    console.log(data);
     const onNavigate = () => {
         if (data.isNode) {
             navigate(data.val.name)
@@ -11,8 +13,13 @@ export default function Component({ data }) {
     }
 
     return (
-        <Flex direction={"column"} py={1} px={2} border={"1px"} m={1} borderColor={"gray.200"} >
-            <Text _hover={{ backgroundColor: "gray.200" }} onClick={onNavigate} cursor={data.isNode ? "pointer" : "default"}>{data.word}</Text>
+        <Flex direction={"column"} py={1} pl={2} border={"1px"} borderColor={"gray.200"} >
+            <Flex py={1} justifyContent={"space-between"} _hover={{ backgroundColor: "gray.200" }}>
+            <Text onClick={onNavigate} cursor={data.isNode ? "pointer" : "default"}>{data.word}</Text>
+                {data.isNode && (
+                    <Amount amount={data.val.snapshot.summary.number} currency={data.val.snapshot.summary.currency}></Amount>
+                )}
+            </Flex>
             <Box pl={2}>
                 {Object.keys(data.children).map(child => (
                     <Component data={data.children[child]} />
