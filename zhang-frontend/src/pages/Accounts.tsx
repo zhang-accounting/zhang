8316fdad55eee4a850bb343a44e2de581273046a
Kilerd <blove694@gmail.com>
2022-04-01 21:32:50 +0800

    feat: add checkbok for hide closed account

--- zhang-frontend/src/pages/Accounts.tsx
+++ zhang-frontend/src/pages/Accounts.tsx
@@ -1,6 +1,7 @@
 import AccountLine from "@/components/AccountLine";
 import { gql, useQuery } from "@apollo/client";
-import { Heading } from "@chakra-ui/react";
+import { Checkbox, Heading, useBoolean } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import AccountTrie from "../utils/AccountTrie";
 
 export default function Accounts() {
@@ -23,30 +24,40 @@ export default function Accounts() {
          
 `);
 
+  const [hideClosedAccount, sethideClosedAccount] = useBoolean(false);
+  const [accountTrie, setAccountTrie] = useState(new AccountTrie());
+
+  useEffect(() => {
+    if (data) {
+      let trie = new AccountTrie();
+      trie.insert({ name: "Assets" });
+      console.log(data.accounts.filter(it => hideClosedAccount ? it.status === "OPEN" : true));
+      for (let account of data.accounts.filter(it => hideClosedAccount ? it.status === "OPEN" : true)) {
+        trie.insert(account);
+      }
+      setAccountTrie(trie);
+    }
+  }, [data, hideClosedAccount]);
+
+
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
 
-  let trie = new AccountTrie();
-  trie.insert({ name: "Assets" });
-  for (let account of data.accounts) {
-    trie.insert(account);
-  }
   return (
 
     <div>
-      {Object.keys(trie.children).sort().map(group => (
-        <div>
-          <Heading>{group}</Heading>
+      <Heading>Accounts </Heading>
+      <Checkbox checked={hideClosedAccount} onChange={sethideClosedAccount.toggle}>Hide Closed Account</Checkbox>
+      {Object.keys(accountTrie.children).sort().map(group => (
+        <div key={group}>
+          <Heading size={"l"}>{group}</Heading>
           <div>
-            {Object.keys(trie.children[group].children).sort().map(item => (
-              <AccountLine data={trie.children[group].children[item]} />
+            {Object.keys(accountTrie.children[group].children).sort().map(item => (
+              <AccountLine data={accountTrie.children[group].children[item]} />
             ))}
           </div>
-
         </div>
-
       ))}
-
     </div>
   )
 }
