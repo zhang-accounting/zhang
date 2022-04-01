ecb4e0314e2a102b7760834baf02809adc161c1d
Kilerd <blove694@gmail.com>
2022-04-02 00:25:31 +0800

    test: add multiple value map test

--- src/core/utils/multi_value_map.rs
+++ src/core/utils/multi_value_map.rs
@@ -24,3 +24,39 @@ impl<Key: Hash + Eq, Value> MultiValueMap<Key, Value> {
             .unwrap_or_default()
     }
 }
+
+#[cfg(test)]
+mod test {
+    use crate::core::utils::multi_value_map::MultiValueMap;
+
+    #[test]
+    fn should_insert() {
+        let mut map: MultiValueMap<i32, i32> = MultiValueMap::default();
+        map.insert(1, 2);
+        assert_eq!(map.inner.contains_key(&1), true);
+        assert_eq!(map.inner.get(&1).unwrap(), &vec![2i32]);
+    }
+    #[test]
+    fn should_insert_multiple_value() {
+        let mut map: MultiValueMap<i32, i32> = MultiValueMap::default();
+        map.insert(1, 2);
+        map.insert(1, 3);
+        assert_eq!(map.inner.contains_key(&1), true);
+        assert_eq!(map.inner.get(&1).unwrap(), &vec![2i32, 3i32]);
+    }
+    #[test]
+    fn should_get_none_given_empty() {
+        let map: MultiValueMap<i32, i32> = MultiValueMap::default();
+        assert_eq!(map.get_one(&1), None);
+        let vec1: Vec<&i32> = vec![];
+        assert_eq!(map.get_all(&1), vec1);
+    }
+    #[test]
+    fn should_get_correct_value() {
+        let mut map: MultiValueMap<i32, i32> = MultiValueMap::default();
+        map.insert(1, 2);
+        map.insert(1, 3);
+        assert_eq!(map.get_one(&1), Some(&2));
+        assert_eq!(map.get_all(&1), vec![&2, &3]);
+    }
+}
