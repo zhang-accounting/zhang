725ace66a7bd9cb33cde4885ce3b83c16254de0e
Kilerd <blove694@gmail.com>
2022-04-02 10:40:48 +0800

    test: add latest map test

--- src/core/utils/latest_map.rs
+++ src/core/utils/latest_map.rs
@@ -42,3 +42,56 @@ impl<Key: Eq + Hash + Clone + Ord, Value> LatestMap<Key, Value> {
         self.date_index.contains(key)
     }
 }
+
+#[cfg(test)]
+mod test {
+    use crate::core::utils::latest_map::LatestMap;
+
+    #[test]
+    fn should_insert() {
+        let mut map: LatestMap<i32, i32> = LatestMap::default();
+        map.insert(1, 2);
+        assert!(map.date_index.contains(&1));
+        assert_eq!(map.data.get(&1), Some(&2));
+    }
+    #[test]
+    fn should_contains_key() {
+        let mut map: LatestMap<i32, i32> = LatestMap::default();
+        map.insert(1, 2);
+        assert!(map.contains_key(&1));
+        assert!(!map.contains_key(&2));
+    }
+
+    #[test]
+    fn should_get_mut() {
+        let mut map: LatestMap<i32, i32> = LatestMap::default();
+        map.insert(1, 2);
+        let value = map.get_mut(&1).unwrap();
+        *value = 3;
+
+        assert_eq!(map.data.get(&1), Some(&3));
+    }
+
+    #[test]
+    fn should_get_latest() {
+        let mut map: LatestMap<i32, i32> = LatestMap::default();
+        map.insert(1, 2);
+        map.insert(10, 20);
+        map.insert(20, 40);
+        map.insert(50, 100);
+
+        assert_eq!(map.get_latest(&0), None);
+        assert_eq!(map.get_latest(&1), Some(&2));
+        assert_eq!(map.get_latest(&3), Some(&2));
+        assert_eq!(map.get_latest(&20), Some(&40));
+        assert_eq!(map.get_latest(&24), Some(&40));
+        assert_eq!(map.get_latest(&1000), Some(&100));
+    }
+    #[test]
+    fn should_work_given_map_is_empty() {
+        let map: LatestMap<i32, i32> = LatestMap::default();
+        assert_eq!(map.get_latest(&0), None);
+        assert_eq!(map.get_latest(&1), None);
+        assert_eq!(map.get_latest(&3), None);
+    }
+}
