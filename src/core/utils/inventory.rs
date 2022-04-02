1788ae4cf83e7f2d55f3e02d8bacaa274c0174aa
Kilerd <blove694@gmail.com>
2022-04-02 17:46:37 +0800

    test: add inventory test

--- src/core/utils/inventory.rs
+++ src/core/utils/inventory.rs
@@ -14,6 +14,44 @@ pub struct Inventory {
     pub(crate) prices: Arc<StdRwLock<DatedPriceGrip>>,
 }
 
+impl Inventory {
+    pub fn add_amount(&mut self, amount: Amount) {
+        let decimal1 = BigDecimal::zero();
+        let x = self.inner.get(&amount.currency).unwrap_or(&decimal1);
+        let decimal = (x).add(&amount.number);
+        self.inner.insert(amount.currency, decimal);
+    }
+
+    pub fn sub_amount(&mut self, amount: Amount) {
+        let decimal1 = BigDecimal::zero();
+        let x = self.inner.get(&amount.currency).unwrap_or(&decimal1);
+        let decimal = (x).sub(&amount.number);
+        self.inner.insert(amount.currency, decimal);
+    }
+    pub fn pin(&self) -> Inventory {
+        self.clone()
+    }
+    pub fn pop(&mut self) -> Option<Amount> {
+        self.inner
+            .drain()
+            .take(1)
+            .next()
+            .map(|(currency, number)| Amount::new(number, currency))
+    }
+    pub fn get(&self, currency: &Currency) -> BigDecimal {
+        self.inner.get(currency).cloned().unwrap_or_else(BigDecimal::zero)
+    }
+    pub fn calculate_to_currency(&self, date: NaiveDateTime, currency: &Currency) -> BigDecimal {
+        let price_guard = self.prices.read().unwrap();
+        let mut sum = BigDecimal::zero();
+        for (each_currency, each_number) in &self.inner {
+            let decimal = price_guard.calculate(&date, each_currency, currency, each_number);
+            sum.add_assign(decimal);
+        }
+        sum
+    }
+}
+
 impl Add for &Inventory {
     type Output = Inventory;
 
@@ -66,40 +104,70 @@ impl DailyAccountInventory {
     }
 }
 
-impl Inventory {
-    pub fn add_amount(&mut self, amount: Amount) {
-        let decimal1 = BigDecimal::zero();
-        let x = self.inner.get(&amount.currency).unwrap_or(&decimal1);
-        let decimal = (x).add(&amount.number);
-        self.inner.insert(amount.currency, decimal);
-    }
+#[cfg(test)]
+mod test {
+    mod inventory {
+        use crate::core::amount::Amount;
+        use crate::core::utils::inventory::Inventory;
+        use bigdecimal::BigDecimal;
+        use std::sync::Arc;
 
-    pub fn sub_amount(&mut self, amount: Amount) {
-        let decimal1 = BigDecimal::zero();
-        let x = self.inner.get(&amount.currency).unwrap_or(&decimal1);
-        let decimal = (x).sub(&amount.number);
-        self.inner.insert(amount.currency, decimal);
-    }
-    pub fn pin(&self) -> Inventory {
-        self.clone()
-    }
-    pub fn pop(&mut self) -> Option<Amount> {
-        self.inner
-            .drain()
-            .take(1)
-            .next()
-            .map(|(currency, number)| Amount::new(number, currency))
-    }
-    pub fn get(&self, currency: &Currency) -> BigDecimal {
-        self.inner.get(currency).cloned().unwrap_or_else(BigDecimal::zero)
-    }
-    pub fn calculate_to_currency(&self, date: NaiveDateTime, currency: &Currency) -> BigDecimal {
-        let price_guard = self.prices.read().unwrap();
-        let mut sum = BigDecimal::zero();
-        for (each_currency, each_number) in &self.inner {
-            let decimal = price_guard.calculate(&date, each_currency, currency, each_number);
-            sum.add_assign(decimal);
+        #[test]
+        fn should_add_amount() {
+            let mut inventory = Inventory {
+                inner: Default::default(),
+                prices: Arc::new(Default::default()),
+            };
+
+            inventory.add_amount(Amount::new(BigDecimal::from(1i32), "CNY"));
+            inventory.add_amount(Amount::new(BigDecimal::from(3i32), "USD"));
+            assert_eq!(inventory.inner.get("CNY").unwrap(), &BigDecimal::from(1i32));
+            assert_eq!(inventory.inner.get("USD").unwrap(), &BigDecimal::from(3i32));
+
+            inventory.add_amount(Amount::new(BigDecimal::from(3i32), "USD"));
+            assert_eq!(inventory.inner.get("USD").unwrap(), &BigDecimal::from(6i32));
         }
-        sum
+
+        #[test]
+        fn should_sub_amount() {
+            let mut inventory = Inventory {
+                inner: Default::default(),
+                prices: Arc::new(Default::default()),
+            };
+            inventory.add_amount(Amount::new(BigDecimal::from(3i32), "USD"));
+
+            inventory.sub_amount(Amount::new(BigDecimal::from(1i32), "CNY"));
+            assert_eq!(inventory.inner.get("CNY").unwrap(), &BigDecimal::from(-1i32));
+
+            inventory.sub_amount(Amount::new(BigDecimal::from(1i32), "USD"));
+            assert_eq!(inventory.inner.get("USD").unwrap(), &BigDecimal::from(2i32));
+        }
+
+        #[test]
+        fn should_get_correct_amount() {
+            let mut inventory = Inventory {
+                inner: Default::default(),
+                prices: Arc::new(Default::default()),
+            };
+            inventory.add_amount(Amount::new(BigDecimal::from(3i32), "USD"));
+            inventory.add_amount(Amount::new(BigDecimal::from(10i32), "CNY"));
+
+            assert_eq!(inventory.get(&"USD".to_string()), BigDecimal::from(3i32));
+            assert_eq!(inventory.get(&"CNY".to_string()), BigDecimal::from(10i32));
+        }
+        #[test]
+        fn should_pin() {
+            let mut inventory = Inventory {
+                inner: Default::default(),
+                prices: Arc::new(Default::default()),
+            };
+            inventory.add_amount(Amount::new(BigDecimal::from(3i32), "USD"));
+
+            let other = inventory.pin();
+            inventory.add_amount(Amount::new(BigDecimal::from(4i32), "USD"));
+
+            assert_eq!(inventory.get(&"USD".to_string()), BigDecimal::from(7i32));
+            assert_eq!(other.get(&"USD".to_string()), BigDecimal::from(3i32));
+        }
     }
 }
