00aa1e605acad4917940c4179c158bd30fc66442
Kilerd <blove694@gmail.com>
2022-04-26 18:28:40 +0800

    feat: add journal side preview content for journal page

--- frontend/src/pages/Journals.tsx
+++ frontend/src/pages/Journals.tsx
@@ -1,22 +1,36 @@
 import { useQuery } from "@apollo/client";
-import { Heading } from '@chakra-ui/react';
+import { Box, CloseButton, Flex, Heading } from '@chakra-ui/react';
+import { useState } from "react";
+import Block from "../components/Block";
 import JournalLine from "../components/JournalLine";
-import { JouralListQuery, JOURNAL_LIST } from "../gql/jouralList";
+import { JouralListQuery, JournalItem, JOURNAL_LIST } from "../gql/jouralList";
 function Journals() {
   const { loading, error, data } = useQuery<JouralListQuery>(JOURNAL_LIST);
-
+  const [selectedJournal, setSelectedJournal] = useState<JournalItem | null>(null)
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
 
 
   return (
-    <div>
-      <Heading mx={4} my={4}>{data?.journals.length} Journals</Heading>
-      <div>
-        {data?.journals.map((journal, idx) => <JournalLine key={idx} data={journal} />)
-        }
-      </div>
-    </div>
+    <Flex>
+      <Box flex="1">
+        <Heading mx={4} my={4}>{data?.journals.length} Journals</Heading>
+        <div>
+          {data?.journals.map((journal, idx) => <JournalLine key={idx} data={journal} setSelectedJournal={setSelectedJournal} />)
+          }
+        </div>
+      </Box>
+      {selectedJournal &&
+        <Box flex=" 0 0 30%">
+          <div><CloseButton onClick={() => setSelectedJournal(null)} /></div>
+          <Heading>{selectedJournal.date}</Heading>
+          <Heading>{selectedJournal.type}</Heading>
+
+        </Box>
+      }
+
+    </Flex>
+
 
   );
 }
