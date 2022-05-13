5334e8c407915e4901ec27b65996e762fae58a08
Kilerd <blove694@gmail.com>
2022-05-13 18:01:53 +0800

    feat: show meta block only when metas is not empty

--- frontend/src/components/journalPreview/TransactionPreview.tsx
+++ frontend/src/components/journalPreview/TransactionPreview.tsx
@@ -11,27 +11,15 @@ export default function TransactionPreview(props: Props) {
     return (
         <div>
             <Box mb={10}>
-            <Box mx={1} my={2}>{format(new Date(props.data.timestamp * 1000), "yyyy-MM-dd hh:mm:ss")}</Box>
-            <Box mx={1} my={2}><Text fontWeight={"bold"}>{props.data.payee}</Text></Box>
-            <Box mx={1} my={2}>{props.data.narration}</Box>
-            <Box mx={1} my={2}>
-                {props.data.tags.map(tag => <Tag borderRadius='full' variant="outline">#{tag}</Tag>)}
-                {props.data.links.map(link => <Tag borderRadius='full' variant="outline" colorScheme='blue'>^{link}</Tag>)}
+                <Box mx={1} my={2}>{format(new Date(props.data.timestamp * 1000), "yyyy-MM-dd hh:mm:ss")}</Box>
+                <Box mx={1} my={2}><Text fontWeight={"bold"}>{props.data.payee}</Text></Box>
+                <Box mx={1} my={2}>{props.data.narration}</Box>
+                <Box mx={1} my={2}>
+                    {props.data.tags.map(tag => <Tag borderRadius='full' variant="outline">#{tag}</Tag>)}
+                    {props.data.links.map(link => <Tag borderRadius='full' variant="outline" colorScheme='blue'>^{link}</Tag>)}
+                </Box>
             </Box>
-            </Box>
-            <Box mx={1} my={4}>
-                <Block title="Metas">
-                    <Box>
-                        {props.data.metas.map((meta, idx) => (
-                            <Flex key={idx} justifyContent="space-between">
-                            <div>{meta.key}</div>
-                            <div>{meta.value}</div>
-                        </Flex>
-                        ))}
 
-                    </Box>
-                </Block>
-            </Box>
             <Box mx={1} my={4}>
                 <Block title="Postings">
                     <>
@@ -42,6 +30,21 @@ export default function TransactionPreview(props: Props) {
                     </>
                 </Block>
             </Box>
+            {
+                props.data.metas.length > 0 && <Box mx={1} my={4}>
+                    <Block title="Metas">
+                        <Box>
+                            {props.data.metas.map((meta, idx) => (
+                                <Flex key={idx} justifyContent="space-between">
+                                    <div>{meta.key}</div>
+                                    <div>{meta.value}</div>
+                                </Flex>
+                            ))}
+
+                        </Box>
+                    </Block>
+                </Box>
+            }
         </div>
     )
 }
