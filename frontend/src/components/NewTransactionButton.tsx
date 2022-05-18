0b869398cbf3b9179b4c8a9a9e5752edc98ce829
Kilerd <blove694@gmail.com>
2022-05-18 17:45:25 +0800

    feat: #37 using selectable for payee input

--- frontend/src/components/NewTransactionButton.tsx
+++ frontend/src/components/NewTransactionButton.tsx
@@ -1,10 +1,12 @@
 import { gql, useMutation, useQuery } from "@apollo/client";
 import { Box, Button, Checkbox, Flex, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useBoolean, useDisclosure } from "@chakra-ui/react";
 import { format } from "date-fns";
+import _ from "lodash";
 import { useState } from "react";
 // @ts-ignore
 import DateTimePicker from 'react-datetime-picker';
 import Select from 'react-select';
+import CreatableSelect from 'react-select/creatable';
 import { AccountItem } from "../gql/accountList";
 
 
@@ -19,10 +21,15 @@ interface SelectMap {
 }
 export default function NewTransactionButton() {
 
-    const accountInfo = useQuery<{ accounts: AccountItem[] }>(gql`
+    const newTransactionMetaData = useQuery<{ accounts: AccountItem[], journals: { payee: string }[] }>(gql`
     query NEW_TRANSACTION_MODAL_DATA {
         accounts {
           name
+        }
+        journals {
+            ... on TransactionDto {
+            payee
+        }
         }
       }
     `)
@@ -39,7 +46,7 @@ export default function NewTransactionButton() {
     const [dateOnly, setDateOnly] = useBoolean(false);
 
     const [date, setDate] = useState(new Date());
-    const [payee, setPayee] = useState("");
+    const [payee, setPayee] = useState<SelectItem | null>(null);
     const [narration, setNarration] = useState("");
     const [postings, setPostings] = useState<Posting[]>([
         { account: null, amount: "" },
@@ -61,7 +68,7 @@ export default function NewTransactionButton() {
         const dateDisplay = format(date, dateOnly ? "yyyy-MM-dd" : "yyyy-MM-dd HH:mm:ss");
         const narrationDisplay = narration.trim().length === 0 ? "" : ` ${JSON.stringify(narration.trim())}`;
         const postingDisplay = postings.map(posting => `  ${posting.account?.value} ${posting.amount}`).join("\n");
-        return `${dateDisplay} ${JSON.stringify(payee)}${narrationDisplay}\n${postingDisplay}`
+        return `${dateDisplay} ${JSON.stringify(payee?.value)}${narrationDisplay}\n${postingDisplay}`
     }
 
     const valid = (): boolean => {
@@ -82,7 +89,7 @@ export default function NewTransactionButton() {
         });
         onClose();
         setDate(new Date());
-        setPayee("");
+        setPayee(null);
         setNarration("");
         setPostings([
             { account: null, amount: "" },
@@ -90,15 +97,29 @@ export default function NewTransactionButton() {
         ]);
     }
 
-    if (accountInfo.loading) return <p>Loading...</p>;
-    if (accountInfo.error) return <p>Error :(</p>;
-    const selectMap = accountInfo.data?.accounts.reduce((ret, singleAccountInfo) => {
+    if (newTransactionMetaData.loading) return <p>Loading...</p>;
+    if (newTransactionMetaData.error) return <p>Error :(</p>;
+
+    const payeeSelectItems: SelectItem[] = _.uniqBy(
+        _.filter(
+            newTransactionMetaData.data!.journals,
+            (journal) => !_.isEmpty(journal.payee)
+        ),
+        (journal) => journal.payee
+    ).map((journal) => {
+        return {
+            label: journal.payee,
+            value: journal.payee
+        }
+    })
+    const selectMap = newTransactionMetaData.data?.accounts.reduce((ret, singleAccountInfo) => {
         const type = singleAccountInfo.name.split(":")[0];
         const item = { label: singleAccountInfo.name, value: singleAccountInfo.name };
         ret[type] = ret[type] || { label: type.toUpperCase(), options: [] };
         ret[type].options.push(item);
         return ret;
     }, {} as SelectMap);
+
     const accountSelectItems = Object.values(selectMap || {}).sort();
 
     return (
@@ -129,8 +150,35 @@ export default function NewTransactionButton() {
                                     <Box m={1}>
                                         <DateTimePicker onChange={setDate} value={date} />
                                     </Box>
-                                    <Box m={1}>
-                                        <Input placeholder='Payee' value={payee} onChange={e => setPayee(e.target.value)} />
+                                    <Box m={1} flex="1">
+                                        <CreatableSelect
+                                            styles={{
+                                                control: (provided, state) => {
+                                                    const borderColor = state.isFocused ? "var(--chakra-colors-gray-300)" : "var(--chakra-colors-gray-200)"
+                                                    return {
+                                                        ...provided,
+                                                        borderWidth: "1px",
+                                                        borderColor: borderColor
+                                                    }
+                                                },
+                                                singleValue: (provided, _state) => ({
+                                                    ...provided,
+                                                    fontSize: "var(--chakra-fontSizes-md)",
+                                                    margin: "5px 7px"
+                                                }),
+                                                placeholder: (provided, _state) => ({
+                                                    ...provided,
+                                                    fontSize: "var(--chakra-fontSizes-md)",
+                                                    color: "#a0aec0",
+                                                    margin: "5px 7px"
+                                                })
+                                            }}
+                                            isClearable
+                                            isSearchable
+                                            value={payee}
+                                            onChange={(value) => setPayee(value)}
+                                            options={payeeSelectItems}
+                                        />
                                     </Box>
                                     <Box m={1}>
                                         <Input placeholder='Narration' value={narration} onChange={e => setNarration(e.target.value)} />
@@ -144,20 +192,20 @@ export default function NewTransactionButton() {
                                         <Box w='80%'>
                                             <Select
                                                 styles={{
-                                                    control: (provided, state) =>{
-                                                        const borderColor = state.isFocused ? "var(--chakra-colors-gray-300)": "var(--chakra-colors-gray-200)"
+                                                    control: (provided, state) => {
+                                                        const borderColor = state.isFocused ? "var(--chakra-colors-gray-300)" : "var(--chakra-colors-gray-200)"
                                                         return {
                                                             ...provided,
                                                             borderWidth: "1px",
                                                             borderColor: borderColor
                                                         }
                                                     },
-                                                    singleValue: (provided, state) => ({
+                                                    singleValue: (provided, _state) => ({
                                                         ...provided,
                                                         fontSize: "var(--chakra-fontSizes-md)",
                                                         margin: "5px 7px"
                                                     }),
-                                                    placeholder: (provided, state) => ({
+                                                    placeholder: (provided, _state) => ({
                                                         ...provided,
                                                         fontSize: "var(--chakra-fontSizes-md)",
                                                         color: "#a0aec0",
