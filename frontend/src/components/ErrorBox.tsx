ffcf79a445a0cf95c53f0abedb74ba0374144837
Kilerd <blove694@gmail.com>
2022-05-31 16:41:09 +0800

    feat: add modal to show and edit ledger error

--- frontend/src/components/ErrorBox.tsx
+++ frontend/src/components/ErrorBox.tsx
@@ -1,47 +1,84 @@
 import { useQuery } from "@apollo/client";
 import { AddIcon, ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
-import { Box, Flex, Text, ButtonGroup, Button, IconButton } from "@chakra-ui/react";
+import { Box, Flex, Text, ButtonGroup, Button, IconButton, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, useDisclosure } from "@chakra-ui/react";
 import { ErrorEntity, ErrorListQuery, ERROR_LIST } from "../gql/errorList";
 import Block from "./Block";
-import { Connection } from "../models";
+import { useState } from "react";
+import CodeMirror from '@uiw/react-codemirror';
 
 export default function ErrorBox() {
+    const { isOpen, onOpen, onClose } = useDisclosure();
     const { data, loading, refetch } = useQuery<ErrorListQuery>(ERROR_LIST);
+    const [selectError, setSelectError] = useState<ErrorEntity | null>(null);
+    const [selectErrorContent, setSelectErrorContent] = useState<string>("");
 
+
+    const toggleError = (error: ErrorEntity) => {
+        setSelectError(error);
+        setSelectErrorContent(error.span.content);
+        onOpen();
+    }
     const fetchNextPage = () => {
         refetch({
             cursor: data?.errors.pageInfo.endCursor
         })
     }
     const fetchPreviousPage = () => {
-        const cursor  = parseInt(data!.errors.pageInfo.startCursor) - 11;
-        if(cursor > 0) {
+        const cursor = parseInt(data!.errors.pageInfo.startCursor) - 11;
+        if (cursor > 0) {
             refetch({
                 cursor: (cursor).toString()
             })
-        }else {
-            refetch({cursor: "-1"})
+        } else {
+            refetch({ cursor: "-1" })
         }
-        
     }
 
     if (loading) return (<div> loading</div>);
     return (
-        <Block title="errors">
-            <Flex flexDirection={"column"}>
-                {data?.errors.edges.map(edge => edge.node).map(error => (
-                    <Box>
-                        <Text whiteSpace={"nowrap"} textOverflow="ellipsis" overflow="hidden">{error.message}</Text>
-                    </Box>
-                ))}
-            </Flex>
+        <>
+            <Modal isOpen={isOpen} size={"4xl"} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>{selectError?.span.filename}:{selectError?.span.start}:{selectError?.span.end}</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <Text>{selectError?.message}</Text>
+                        <CodeMirror
+                            value={selectErrorContent}
+                            height="20vh"
+                            width="100%"
+                            onChange={(value) => {
+                                setSelectErrorContent(value)
+                            }}
+                        />
+                    </ModalBody>
 
-            <Flex justifyContent={"end"} mt={3}>
-                <ButtonGroup size='sm' isAttached variant='outline'>
-                    <IconButton disabled={!data?.errors.pageInfo.hasPreviousPage} onClick={fetchPreviousPage} aria-label='Add to friends' icon={<ChevronLeftIcon />} />
-                    <IconButton disabled={!data?.errors.pageInfo.hasNextPage} onClick={fetchNextPage} aria-label='Add to friends' icon={<ChevronRightIcon />} />
-                </ButtonGroup>
-            </Flex>
-        </Block>
+                    <ModalFooter>
+                        <Button colorScheme='blue' mr={3} onClick={onClose}>
+                            Close
+                        </Button>
+                        <Button variant='ghost'>Secondary Action</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+            <Block title="errors">
+                <Flex flexDirection={"column"}>
+                    {data?.errors.edges.map(edge => edge.node).map(error => (
+                        <Box onClick={() => toggleError(error)} cursor="pointer">
+                            <Text whiteSpace={"nowrap"} textOverflow="ellipsis" overflow="hidden">{error.message}</Text>
+                        </Box>
+                    ))}
+                </Flex>
+
+                <Flex justifyContent={"end"} mt={3}>
+                    <ButtonGroup size='sm' isAttached variant='outline'>
+                        <IconButton disabled={!data?.errors.pageInfo.hasPreviousPage} onClick={fetchPreviousPage} aria-label='Add to friends' icon={<ChevronLeftIcon />} />
+                        <IconButton disabled={!data?.errors.pageInfo.hasNextPage} onClick={fetchNextPage} aria-label='Add to friends' icon={<ChevronRightIcon />} />
+                    </ButtonGroup>
+                </Flex>
+            </Block>
+        </>
+
     )
 }
