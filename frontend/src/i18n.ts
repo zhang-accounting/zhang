64923cb5ea67f9a7773bc5948f3b77f51e85f1b6
Kilerd Chan <blove694@gmail.com>
2022-08-04 10:09:06 +0000

    fix: process instance access issue

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -1,14 +1,15 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
+// eslint-disable-line
+const isDevelopment = process.env.NODE_ENV === 'development';
 i18n
     .use(Backend)
     .use(initReactI18next)
     .init({
         lng: 'en',
         fallbackLng: 'en',
-        debug: process?.env?.NODE_ENV === 'development',
-
+        debug: isDevelopment,
         interpolation: {
             escapeValue: false, // not needed for react as it escapes by default
         },
