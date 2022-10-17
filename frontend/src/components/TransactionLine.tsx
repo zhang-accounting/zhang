3ab4db4dc0003bd1a8f154c7c55340b661eb89ce
Kilerd Chan <blove694@gmail.com>
2022-10-17 08:51:18 +0000

    feat: show summary color base on amount sign

--- frontend/src/components/TransactionLine.tsx
+++ frontend/src/components/TransactionLine.tsx
@@ -1,15 +1,33 @@
-import { Group, Text } from '@mantine/core';
+import { Group, Text, createStyles } from '@mantine/core';
 import { JournalItem, TransactionDto } from '../gql/jouralList';
 import { IconArrowBigDownLines, IconArrowBigUpLines } from '@tabler/icons';
 import { format } from 'date-fns';
 import { Dispatch, SetStateAction } from 'react';
 import { calculate } from '../utils/trx-calculator';
+
+const useStyles = createStyles((theme) => ({
+  positiveAmount: {
+    color: theme.colors.green[8],
+    fontWeight: 'bold',
+    fontFeatureSettings: 'tnum',
+    fontSize: theme.fontSizes.sm,
+  },
+  negativeAmount: {
+    color: theme.colors.red[8],
+    fontWeight: 'bold',
+    fontFeatureSettings: 'tnum',
+    fontSize: theme.fontSizes.sm,
+  },
+}));
+
 interface Props {
   data: TransactionDto;
   onClick?: Dispatch<SetStateAction<JournalItem | undefined>>;
 }
 
 export default function TransactionLine({ data, onClick }: Props) {
+  const { classes } = useStyles();
+
   const date = format(data.timestamp * 1000, 'yyyy-MM-dd');
   const time = format(data.timestamp * 1000, 'hh:mm:ss');
   const trClick = () => {
@@ -39,10 +57,10 @@ export default function TransactionLine({ data, onClick }: Props) {
       <td>
         <Group spacing="xs" position="right">
           {Array.from(summary.values()).map((each) => (
-            <Group align="center" spacing="xs">
+            <Group align="center" spacing="xs" className={each.number.isPositive() ? classes.positiveAmount : classes.negativeAmount}>
               {each.number.isPositive() ? <IconArrowBigDownLines stroke={1.5} /> : <IconArrowBigUpLines stroke={1.5} />}
               <span>
-                {each.number.abs().toFixed()} {each.currency}
+                {each.number.toFixed()} {each.currency}
               </span>
             </Group>
           ))}
