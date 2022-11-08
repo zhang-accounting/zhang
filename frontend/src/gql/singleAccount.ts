6e0b4534c2384be23383a30310861ed72b4e0f89
Kilerd Chan <blove694@gmail.com>
2022-11-08 06:06:07 +0000

    fix: tags and links for single account request

--- frontend/src/gql/singleAccount.ts
+++ frontend/src/gql/singleAccount.ts
@@ -34,6 +34,8 @@ export const SINGLE_ACCONT_JOURNAL = gql`
           timestamp
           payee
           narration
+          links
+          tags
           postings {
             account {
               name
