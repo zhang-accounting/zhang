36a3f29251b6de966bf1117549850e3370516704
Kilerd Chan <blove694@gmail.com>
2023-04-14 18:31:26 +0800

    feat: impl grid document box

--- frontend/src/components/AccountDocumentUpload.tsx
+++ frontend/src/components/AccountDocumentUpload.tsx
@@ -16,13 +16,12 @@ export default function AccountDocumentUpload(props: Props) {
     if (files.length > 0) {
       const formData = new FormData();
       files.forEach((file) => formData.append('file', file));
-
       axiosInstance
         .post(props.url, formData, {
           headers: {
             'Content-Type': 'multipart/form-data',
           },
-          onUploadProgress: () => {},
+          onUploadProgress: () => { },
         })
         .then(() => {
           setFiles([]);
@@ -37,23 +36,20 @@ export default function AccountDocumentUpload(props: Props) {
   ));
 
   return (
-    <Dropzone onDrop={setFiles} radius="sm" maxSize={30 * 1024 ** 2}>
-      {filesDom.length > 0 ? (
-        <div>
-          <ul>{filesDom}</ul>
-        </div>
-      ) : (
-        <div style={{ pointerEvents: 'none' }}>
-          <Text align="center" weight={700} size="lg" mt="md">
-            <Dropzone.Accept>Drop files here</Dropzone.Accept>
-            <Dropzone.Reject>Pdf file less than 30mb</Dropzone.Reject>
-            <Dropzone.Idle>Upload Documents</Dropzone.Idle>
-          </Text>
-          <Text align="center" size="sm" mt="xs" color="dimmed">
-            Drag&apos;n&apos;drop files here to upload. We can accept only <i>.pdf</i> files that are less than 30mb in size.
-          </Text>
-        </div>
-      )}
-    </Dropzone>
+      <Dropzone style={{height:"121px", width:"121px"}} onDrop={setFiles} radius="sm" maxSize={30 * 1024 ** 2}>
+        {filesDom.length > 0 ? (
+          <div>
+            <ul>{filesDom}</ul>
+          </div>
+        ) : (
+          <div style={{ pointerEvents: 'none' }}>
+            <Text align="center" weight={700} size="md" mt="md">
+              <Dropzone.Accept>Drop files here</Dropzone.Accept>
+              <Dropzone.Reject>Pdf file less than 30mb</Dropzone.Reject>
+              <Dropzone.Idle>Upload Document</Dropzone.Idle>
+            </Text>
+          </div>
+        )}
+      </Dropzone>
   );
 }
--- frontend/src/components/journalPreview/DocumentPreview.tsx
+++ frontend/src/components/journalPreview/DocumentPreview.tsx
@@ -1,15 +1,23 @@
-// @ts-ignore
-import { ActionIcon, Button, Text } from '@mantine/core';
-import { IconFileDownload, IconZoomInArea } from '@tabler/icons';
-import DashLine from '../DashedLine';
-import { EXTENSIONS_SUPPORT_PREVIEW } from '../documentLines/AccountDocumentLine';
+import { createStyles, Image, Text } from '@mantine/core';
 import { openContextModal } from '@mantine/modals';
+import { Buffer } from 'buffer';
+import { serverBaseUrl } from '../..';
+import { EXTENSIONS_SUPPORT_PREVIEW } from '../documentLines/AccountDocumentLine';
 
+const useStyles = createStyles((theme, _params, getRef) => ({
+  img: {
+    '&:hover': {
+      cursor: 'pointer',
+    },
+  },
+
+}));
 interface Props {
   uri: string;
   filename: string;
 }
 export default function DocumentPreview({ filename }: Props) {
+  const {classes} = useStyles();
   const extension = filename.split('.').pop()?.toUpperCase() || '';
   const simpleFilename = filename.split('/').pop() || '';
   const canPreview = EXTENSIONS_SUPPORT_PREVIEW.includes(extension);
@@ -27,21 +35,17 @@ export default function DocumentPreview({ filename }: Props) {
     });
   };
   return (
-    <DashLine>
-      <Text lineClamp={1} my="xs">
-        {simpleFilename}
-      </Text>
-
-      <Button.Group>
-        {canPreview && (
-          <ActionIcon onClick={openDocumentModal}>
-            <IconZoomInArea size={18} />
-          </ActionIcon>
-        )}
-        <ActionIcon>
-          <IconFileDownload size={18} />
-        </ActionIcon>
-      </Button.Group>
-    </DashLine>
+    <Image
+      className={classes.img}
+      height={"121px"}
+      width={"121px"}
+      radius={"sm"}
+      onClick={openDocumentModal}
+      src={canPreview ? `${serverBaseUrl}/api/documents/${Buffer.from(filename).toString('base64')}`: ""}
+      fit='cover'
+      alt="With custom placeholder"
+      withPlaceholder
+      placeholder={<Text align="center">This document cannot be previewed</Text>}
+    />
   );
 }
