7e943415b5b4a67f21914799d09fca87d972447c
Kilerd Chan <blove694@gmail.com>
2023-04-16 18:12:48 +0800

    feat: add balance pad to account

--- frontend/src/components/AccountBalanceCheckLine.tsx
+++ frontend/src/components/AccountBalanceCheckLine.tsx
@@ -1,28 +1,34 @@
-import {TextInput, Button} from '@mantine/core';
+import {TextInput, Button, Select, Group} from '@mantine/core';
 import {useState} from 'react';
 import {axiosInstance} from "../index";
 import {showNotification} from "@mantine/notifications";
-import {useAppDispatch} from "../states";
-import {accountsSlice} from "../states/account";
+import {useAppDispatch, useAppSelector} from "../states";
+import {accountsSlice, getAccountSelectItems} from "../states/account";
+import Amount from "./Amount";
 
 interface Props {
-  currency: string;
+  currentAmount: string,
+  commodity: string;
   accountName: string;
 }
 
-export default function AccountBalanceCheckLine({currency, accountName}: Props) {
+export default function AccountBalanceCheckLine({currentAmount, commodity, accountName}: Props) {
   const [amount, setAmount] = useState('');
+  const [padAccount, setPadAccount] = useState<string | null>(null);
   const dispatch = useAppDispatch();
+
+  const accountItems = [...useAppSelector(getAccountSelectItems())];
 
   const onSave = async () => {
     try {
       await axiosInstance.post(`/api/accounts/${accountName}/balances`, {
-        type: "Balance",
+        type: padAccount ? 'Pad' : "Balance",
         account_name: accountName,
         amount: {
           number: amount,
-          commodity: currency,
-        }
+          commodity: commodity,
+        },
+        pad: padAccount
       });
       showNotification({
         title: 'Balance account successfully',
@@ -47,17 +53,36 @@ export default function AccountBalanceCheckLine({currency, accountName}: Props) 
   };
   return (
       <>
-        <TextInput
-            placeholder={`Balanced ${currency} Amount`}
-            value={amount}
-            onChange={(e) => setAmount(e.target.value)}
-            rightSectionWidth={75}
-            rightSection={
-              <Button size="xs" onClick={submitCheck} disabled={amount.length === 0}>
-                Check
+
+        <tr>
+          <td>{commodity}</td>
+          <td>
+            <Amount amount={currentAmount} currency={commodity}/>
+          </td>
+          <td>{}</td>
+          <td><Select
+              searchable
+              clearable
+              placeholder="Pad to"
+              data={accountItems}
+              value={padAccount}
+              onChange={(e) => setPadAccount(e)}
+          /></td>
+          <td>
+            <Group spacing={"xs"}>
+              <TextInput
+                  placeholder={`Balanced ${commodity} Amount`}
+                  value={amount}
+                  onChange={(e) => setAmount(e.target.value)}
+              ></TextInput>
+              <Button size="sm" onClick={submitCheck} disabled={amount.length === 0}>
+                {padAccount ? "Pad" : "Balance"}
               </Button>
-            }
-        ></TextInput>
+            </Group>
+
+          </td>
+        </tr>
+
       </>
   );
 }
--- frontend/src/pages/SingleAccount.tsx
+++ frontend/src/pages/SingleAccount.tsx
@@ -1,117 +1,118 @@
-import { Container, Group, Table, Tabs, Title } from '@mantine/core';
-import { IconMessageCircle, IconPhoto, IconSettings } from '@tabler/icons';
-import { format } from 'date-fns';
-import { useParams } from 'react-router';
+import {Container, Group, Table, Tabs, Title} from '@mantine/core';
+import {IconMessageCircle, IconPhoto, IconSettings} from '@tabler/icons';
+import {format} from 'date-fns';
+import {useParams} from 'react-router';
 import AccountDocumentUpload from '../components/AccountDocumentUpload';
 import LoadingComponent from '../components/basic/LoadingComponent';
 import AccountDocumentLine from '../components/documentLines/AccountDocumentLine';
-import { AccountJournalItem, Document } from '../rest-model';
-import {useAppSelector} from "../states";
-import {getAccountByName} from "../states/account";
-import Amount from "../components/Amount";
+import {AccountJournalItem, Document, LoadingState} from '../rest-model';
+import {useAppDispatch, useAppSelector} from "../states";
+import {fetchAccounts, getAccountByName} from "../states/account";
 import AccountBalanceCheckLine from "../components/AccountBalanceCheckLine";
+import {useEffect} from "react";
 
 function SingleAccount() {
-  let { accountName } = useParams();
+  let {accountName} = useParams();
+  const dispatch = useAppDispatch();
+
   const account = useAppSelector(getAccountByName(accountName!));
+  const accountStatus = useAppSelector((state) => state.accounts.status);
+
+  useEffect(() => {
+    if (accountStatus === LoadingState.NotReady) {
+      dispatch(fetchAccounts());
+    }
+  }, [dispatch, accountStatus]);
 
   return (
-    <Container fluid>
-      <Title order={2}>{accountName}</Title>
-      <Group>{/* <Badge variant="outline">{data?.account.status}</Badge> */}</Group>
-      <Tabs defaultValue="journals" mt="lg">
-        <Tabs.List>
-          <Tabs.Tab value="journals" icon={<IconPhoto size={14} />}>
-            Journals
-          </Tabs.Tab>
-          <Tabs.Tab value="documents" icon={<IconMessageCircle size={14} />}>
-            Documents
-          </Tabs.Tab>
-          <Tabs.Tab value="settings" icon={<IconSettings size={14} />}>
-            Settings
-          </Tabs.Tab>
-        </Tabs.List>
+      <Container fluid>
+        <Title order={2}>{accountName}</Title>
+        <Group>{/* <Badge variant="outline">{data?.account.status}</Badge> */}</Group>
+        <Tabs defaultValue="journals" mt="lg">
+          <Tabs.List>
+            <Tabs.Tab value="journals" icon={<IconPhoto size={14}/>}>
+              Journals
+            </Tabs.Tab>
+            <Tabs.Tab value="documents" icon={<IconMessageCircle size={14}/>}>
+              Documents
+            </Tabs.Tab>
+            <Tabs.Tab value="settings" icon={<IconSettings size={14}/>}>
+              Settings
+            </Tabs.Tab>
+          </Tabs.List>
 
-        <Tabs.Panel value="journals" pt="xs">
-          <Table verticalSpacing="xs" highlightOnHover>
-            <thead>
+          <Tabs.Panel value="journals" pt="xs">
+            <Table verticalSpacing="xs" highlightOnHover>
+              <thead>
               <tr>
                 <th>Date</th>
                 <th>Payee & Narration</th>
                 <th>Change Amount</th>
                 <th>After Change Amount</th>
               </tr>
-            </thead>
-            <tbody>
+              </thead>
+              <tbody>
               <LoadingComponent
-                url={`/api/accounts/${accountName}/journals`}
-                skeleton={<div>loading</div>}
-                render={(data: AccountJournalItem[]) => (
-                  <>
-                    {data.map((item) => (
-                      <tr>
-                        <td>{format(new Date(item.datetime), 'yyyy-MM-dd hh:mm:ss')}</td>
-                        <td>
-                          {item.payee} {item.narration}
-                        </td>
-                        <td>
-                          {item.inferred_unit_number} {item.inferred_unit_commodity}
-                        </td>
-                        <td>
-                          {item.account_after_number} {item.account_after_commodity}
-                        </td>
-                      </tr>
-                    ))}
-                  </>
-                )}
+                  url={`/api/accounts/${accountName}/journals`}
+                  skeleton={<div>loading</div>}
+                  render={(data: AccountJournalItem[]) => (
+                      <>
+                        {data.map((item) => (
+                            <tr>
+                              <td>{format(new Date(item.datetime), 'yyyy-MM-dd hh:mm:ss')}</td>
+                              <td>
+                                {item.payee} {item.narration}
+                              </td>
+                              <td>
+                                {item.inferred_unit_number} {item.inferred_unit_commodity}
+                              </td>
+                              <td>
+                                {item.account_after_number} {item.account_after_commodity}
+                              </td>
+                            </tr>
+                        ))}
+                      </>
+                  )}
               />
-            </tbody>
-          </Table>
-        </Tabs.Panel>
+              </tbody>
+            </Table>
+          </Tabs.Panel>
 
-        <Tabs.Panel value="documents" pt="xs">
-          <LoadingComponent
-            url={`/api/accounts/${accountName}/documents`}
-            skeleton={<div>loading</div>}
-            render={(data: Document[]) => (
-              <>
-                <AccountDocumentUpload url={`/api/accounts/${accountName}/documents`} />
-                {data.map((document, idx) => (
-                  <AccountDocumentLine key={idx} {...document} />
-                ))}
-              </>
-            )}
-          ></LoadingComponent>
-        </Tabs.Panel>
+          <Tabs.Panel value="documents" pt="xs">
+            <LoadingComponent
+                url={`/api/accounts/${accountName}/documents`}
+                skeleton={<div>loading</div>}
+                render={(data: Document[]) => (
+                    <>
+                      <AccountDocumentUpload url={`/api/accounts/${accountName}/documents`}/>
+                      {data.map((document, idx) => (
+                          <AccountDocumentLine key={idx} {...document} />
+                      ))}
+                    </>
+                )}
+            ></LoadingComponent>
+          </Tabs.Panel>
 
-        <Tabs.Panel value="settings" pt="xs">
-          <Table verticalSpacing="xs" highlightOnHover>
-            <thead>
+          <Tabs.Panel value="settings" pt="xs">
+            <Table verticalSpacing="xs" highlightOnHover>
+              <thead>
               <tr>
                 <th>Currency</th>
                 <th>Current Balance</th>
                 <th>Latest Balance Time</th>
+                <th>Pad Account</th>
                 <th>Distanation</th>
               </tr>
-            </thead>
-            <tbody>
-              {Object.entries(account.commodities).map(([key, value], idx) => (
-                <tr key={idx}>
-                  <td>{key}</td>
-                  <td>
-                    <Amount amount={value} currency={key} />
-                  </td>
-                  <td>{}</td>
-                  <td>
-                    <AccountBalanceCheckLine currency={key} accountName={account.name} />
-                  </td>
-                </tr>
+              </thead>
+              <tbody>
+              {Object.entries(account?.commodities??[]).map(([commodity, amount], idx) => (
+                  <AccountBalanceCheckLine currentAmount={amount} commodity={commodity} accountName={account.name}/>
               ))}
-            </tbody>
-          </Table>
-        </Tabs.Panel>
-      </Tabs>
-    </Container>
+              </tbody>
+            </Table>
+          </Tabs.Panel>
+        </Tabs>
+      </Container>
   );
 }
 
