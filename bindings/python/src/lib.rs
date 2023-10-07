66b6d9b9c71924e26c6e1dc664aae81da2c8c376
Kilerd Chan <blove694@gmail.com>
2023-10-07 23:16:49 +0800

    feat: add ledger parse methods

--- bindings/python/src/lib.rs
+++ bindings/python/src/lib.rs
@@ -1,22 +1,46 @@
+use std::env::temp_dir;
+use std::path::PathBuf;
+use std::str::FromStr;
 use pyo3::prelude::*;
-use pyo3::types::PyString;
 use zhang_ast::{Spanned};
 
 use zhang_core::text::parser::parse;
+use zhang_core::text::transformer::TextTransformer;
+
 /// Formats the sum of two numbers as string.
 
 #[pyclass]
 pub struct Directive(Spanned<zhang_ast::Directive>);
 
 
-#[pyfunction]
-fn parse_content(content: &str)-> PyResult<Vec<Directive>> {
-    Ok(parse(content, None).unwrap().into_iter().map(Directive).collect())
+#[pyclass]
+pub struct Ledger(zhang_core::ledger::Ledger);
+
+#[pymethods]
+impl Ledger {
+
+    #[new]
+    pub fn new(path: &str, endpoint: &str) -> PyResult<Self> {
+        let pathbuf = PathBuf::from_str(path)?;
+        Ok(Ledger(zhang_core::ledger::Ledger::load::<TextTransformer>(pathbuf, endpoint.to_owned()).unwrap()))
+    }
+
+    #[staticmethod]
+    pub fn from_str(content: &str) -> PyResult<Self> {
+        let t_dir = temp_dir();
+        let endpoint = t_dir.join("main.zhang");
+        std::fs::write( endpoint, content)?;
+        Ok(Ledger(zhang_core::ledger::Ledger::load::<TextTransformer>(t_dir, "main.zhang".to_owned()).unwrap()))
+
+    }
 }
+
+
+
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn zhang(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(parse_content, m)?)?;
+    m.add_class::<Ledger>()?;
     Ok(())
 }
