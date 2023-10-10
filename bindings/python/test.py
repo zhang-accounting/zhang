b3f9807bd042968ffc350b0338624d29c7e88ef8
Kilerd Chan <blove694@gmail.com>
2023-10-10 16:48:57 +0800

    test: format python test code

--- bindings/python/test.py
+++ bindings/python/test.py
@@ -3,18 +3,17 @@ from zhang import Ledger
 from pprint import pprint
 from terminaltables import AsciiTable
 
+
+def print_as_table(header, data):
+    table = [header]
+    table.extend(data)
+    print(AsciiTable(options).table)
+
 print("loading examples/example.zhang")
 ledger = Ledger("../../examples", "example.zhang")
-print("printing options")
-options = [
- ["option key", "option value"],
-]
-options.extend([[key, ledger.options[key]] for key in ledger.options])
-print(AsciiTable(options).table)
 
-accounts = [
-    ["Account name", "type", "status", "alias"]
-]
-accounts.extend([[account.name, account.type, account.status, account.alias] for account in ledger.accounts.values()])
+options = [[key, ledger.options[key]] for key in ledger.options]
+print_as_table(["option key", "option value"], options)
 
-print(AsciiTable(accounts).table)
+accounts = [[account.name, account.type, account.status, account.alias] for account in ledger.accounts.values()]
+print_as_table(["Account name", "type", "status", "alias"], accounts)
