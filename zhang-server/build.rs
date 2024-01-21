46623d40960f1e5c6d5102e3e94400b830d8ed14
Kilerd Chan <blove694@gmail.com>
2024-01-22 00:23:30 +0800

    fix: version file generate script

--- zhang-server/build.rs
+++ zhang-server/build.rs
@@ -2,7 +2,10 @@ use chrono::Local;
 
 fn main() {
     let now = Local::now();
-    let build_version = std::fs::read_to_string("../.build_version").unwrap_or(env!("CARGO_PKG_VERSION").to_string());
+    let build_version = std::fs::read_to_string("../.build_version")
+        .unwrap_or(env!("CARGO_PKG_VERSION").to_string())
+        .trim()
+        .to_string();
     println!("cargo:rerun-if-changed=../.build_version");
     println!("cargo:rustc-env=ZHANG_BUILD_DATE={}", now.format("%Y-%m-%d"));
     println!("cargo:rustc-env=ZHANG_BUILD_VERSION={}", build_version);
