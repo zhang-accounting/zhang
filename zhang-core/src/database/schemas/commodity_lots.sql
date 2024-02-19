ef09cb480309855d2152a64287760f5821875e5d
Kilerd Chan <blove694@gmail.com>
2024-02-20 00:15:27 +0800

    refact: remove database relative files

--- zhang-core/src/database/schemas/commodity_lots.sql
+++
@@ -1,9 +0,0 @@
-create table if not exists commodity_lots
-(
-    commodity       varchar not null,
-    datetime        datetime,
-    amount          REAL,
-    price_amount    REAL,
-    price_commodity varchar,
-    account         varchar
-);
