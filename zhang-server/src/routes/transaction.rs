0e1b03be4edc630261a597d604a0daacfc2ead82
Kilerd Chan <blove694@gmail.com>
2024-02-26 17:30:45 +0800

    refact: use postings in txn directly

--- zhang-server/src/routes/transaction.rs
+++ zhang-server/src/routes/transaction.rs
@@ -1,10 +1,8 @@
-use std::collections::{HashMap, HashSet};
 use std::str::FromStr;
 use std::sync::Arc;
 
 use axum::extract::{Multipart, Path, Query, State};
 use axum::Json;
-use chrono::NaiveDateTime;
 use indexmap::IndexSet;
 use itertools::Itertools;
 use log::info;
@@ -15,6 +13,7 @@ use zhang_ast::amount::Amount;
 use zhang_ast::{Account, Date, Directive, Flag, Meta, Posting, Transaction, ZhangString};
 use zhang_core::domains::schemas::MetaType;
 use zhang_core::ledger::Ledger;
+use zhang_core::store::TransactionDomain;
 use zhang_core::utils::string_::StringExt;
 
 use crate::request::{CreateTransactionRequest, JournalRequest};
@@ -45,18 +44,9 @@ pub async fn get_journals(ledger: State<Arc<RwLock<Ledger>>>, params: Query<Jour
 
     let total_count = operations.transaction_counts()?;
 
-    #[derive(Debug)]
-    struct JournalHeader {
-        id: Uuid,
-        sequence: i32,
-        datetime: NaiveDateTime,
-        journal_type: String,
-        payee: String,
-        narration: Option<String>,
-    }
     let store = operations.read();
 
-    let journal_headers = store
+    let journals: Vec<TransactionDomain> = store
         .transactions
         .values()
         .filter(|it| params.keyword.as_ref().map(|keyword| it.contains_keyword(keyword)).unwrap_or(true))
@@ -64,34 +54,16 @@ pub async fn get_journals(ledger: State<Arc<RwLock<Ledger>>>, params: Query<Jour
         .skip(params.offset() as usize)
         .take(params.limit() as usize)
         .cloned()
-        .map(|it| JournalHeader {
-            id: it.id,
-            sequence: it.sequence,
-            datetime: it.datetime.naive_local(),
-            journal_type: it.flag.to_string(),
-            payee: it.payee.unwrap_or_default(),
-            narration: it.narration,
-        })
-        .collect_vec();
-
-    let header_ids: HashSet<Uuid> = journal_headers.iter().map(|it| it.id).collect();
-
-    let postings = store
-        .postings
-        .iter()
-        .filter(|posting| header_ids.contains(&posting.trx_id))
-        .cloned()
         .collect_vec();
 
     drop(store);
-    let mut header_map: HashMap<Uuid, JournalHeader> = journal_headers.into_iter().map(|it| (it.id.to_owned(), it)).collect();
-
     let mut ret = vec![];
-    for (trx_id, arms) in &postings.into_iter().group_by(|it| it.trx_id.to_owned()) {
-        let header = header_map.remove(&trx_id).expect("cannot found trx header");
-        let item = match header.journal_type.as_str() {
-            "BalancePad" => {
-                let postings = arms
+    for journal_item in journals {
+        let item = match journal_item.flag {
+            Flag::BalancePad => {
+                let postings = journal_item
+                    .postings
+                    .into_iter()
                     .map(|arm| JournalTransactionPostingResponse {
                         account: arm.account.name().to_owned(),
                         unit_number: arm.unit.as_ref().map(|it| it.number.clone()),
@@ -107,17 +79,19 @@ pub async fn get_journals(ledger: State<Arc<RwLock<Ledger>>>, params: Query<Jour
                     })
                     .collect_vec();
                 JournalItemResponse::BalancePad(JournalBalancePadItemResponse {
-                    id: trx_id,
-                    sequence: header.sequence,
-                    datetime: header.datetime,
-                    payee: header.payee,
-                    narration: header.narration,
-                    type_: header.journal_type,
+                    id: journal_item.id,
+                    sequence: journal_item.sequence,
+                    datetime: journal_item.datetime.naive_local(),
+                    payee: journal_item.payee.unwrap_or_default(),
+                    narration: journal_item.narration,
+                    type_: journal_item.flag.to_string(),
                     postings,
                 })
             }
-            "BalanceCheck" => {
-                let postings = arms
+            Flag::BalanceCheck => {
+                let postings = journal_item
+                    .postings
+                    .into_iter()
                     .map(|arm| JournalTransactionPostingResponse {
                         account: arm.account.name().to_owned(),
                         unit_number: arm.unit.as_ref().map(|it| it.number.clone()),
@@ -133,17 +107,19 @@ pub async fn get_journals(ledger: State<Arc<RwLock<Ledger>>>, params: Query<Jour
                     })
                     .collect_vec();
                 JournalItemResponse::BalanceCheck(JournalBalanceCheckItemResponse {
-                    id: trx_id,
-                    sequence: header.sequence,
-                    datetime: header.datetime,
-                    payee: header.payee,
-                    narration: header.narration,
-                    type_: header.journal_type,
+                    id: journal_item.id,
+                    sequence: journal_item.sequence,
+                    datetime: journal_item.datetime.naive_local(),
+                    payee: journal_item.payee.unwrap_or_default(),
+                    narration: journal_item.narration,
+                    type_: journal_item.flag.to_string(),
                     postings,
                 })
             }
             _ => {
-                let postings = arms
+                let postings = journal_item
+                    .postings
+                    .into_iter()
                     .map(|arm| JournalTransactionPostingResponse {
                         account: arm.account.name().to_owned(),
                         unit_number: arm.unit.as_ref().map(|it| it.number.clone()),
@@ -158,23 +134,23 @@ pub async fn get_journals(ledger: State<Arc<RwLock<Ledger>>>, params: Query<Jour
                         account_after_commodity: arm.after_amount.currency,
                     })
                     .collect_vec();
-                let tags = operations.trx_tags(trx_id.to_string())?;
-                let links = operations.trx_links(trx_id.to_string())?;
+                let tags = operations.trx_tags(journal_item.id.to_string())?;
+                let links = operations.trx_links(journal_item.id.to_string())?;
                 let metas = operations
-                    .metas(MetaType::TransactionMeta, trx_id.to_string())
+                    .metas(MetaType::TransactionMeta, journal_item.id.to_string())
                     .unwrap()
                     .into_iter()
                     .map(|it| it.into())
                     .collect();
                 JournalItemResponse::Transaction(JournalTransactionItemResponse {
-                    id: trx_id,
-                    sequence: header.sequence,
-                    datetime: header.datetime,
-                    payee: header.payee,
-                    narration: header.narration,
+                    id: journal_item.id,
+                    sequence: journal_item.sequence,
+                    datetime: journal_item.datetime.naive_local(),
+                    payee: journal_item.payee.unwrap_or_default(),
+                    narration: journal_item.narration,
                     tags,
                     links,
-                    flag: header.journal_type,
+                    flag: journal_item.flag.to_string(),
                     is_balanced: true,
                     postings,
                     metas,
