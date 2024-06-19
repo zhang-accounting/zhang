2c8d3257e46c44199ae7ad3dd90ae9d136ff4639
Kilerd Chan <blove694@gmail.com>
2024-06-19 11:50:57 +0800

    chore: bump to v0.1.10
    Signed-off-by: Kilerd Chan <blove694@gmail.com>

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
   integrations: [
     starlight({
-      title: 'Zhang Accounting',
+      title: 'Zhang Accounting v0.1.10',
       head: [{
         tag: 'script',
         attrs: {
