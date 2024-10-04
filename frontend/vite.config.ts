c8040c889b527aa6da264901fd1c8ed508659256
Kilerd Chan <blove694@gmail.com>
2024-10-05 01:47:32 +0800

    feat: integrate with shadcn/ui

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteCompression from 'vite-plugin-compression';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react(), viteCompression({
@@ -12,7 +13,7 @@ export default defineConfig({
   })],
   resolve: {
     alias: {
-      '@': '/src',
+      '@': path.resolve(__dirname, './src'),
     },
   },
   server: {
