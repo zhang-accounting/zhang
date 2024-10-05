b45afab62f2b564f8a3c270daf68c6f24607b206
Kilerd Chan <blove694@gmail.com>
2024-10-05 14:54:29 +0800

    feat: doucment page

--- frontend/src/components/documentLines/AccountDocumentLine.tsx
+++ frontend/src/components/documentLines/AccountDocumentLine.tsx
@@ -1,92 +1,37 @@
-import { Box, Card, Text } from '@mantine/core';
 import { Buffer } from 'buffer';
 import { Document } from '../../rest-model';
-import { createStyles } from '@mantine/emotion';
 import { isDocumentAnImage } from '../../utils/documents';
 import { serverBaseUrl } from '../../global.ts';
 
-const useStyles = createStyles((theme, _, u) => ({
-  imgBox: {
-    overflow: 'hidden',
-    position: 'relative',
-    '&:after': {
-      content: '" "',
-      display: 'block',
-      paddingBottom: '75%',
-    },
-  },
-  img: {
-    '&:hover': {
-      cursor: 'pointer',
-    },
-    position: 'absolute',
-    // top: theme.spacing.xs,
-    // left: theme.spacing.xs,
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    width: '100%',
-    height: '100%',
-    objectFit: 'cover',
-  },
-
-  empty: {
-    '&:hover': {
-      cursor: 'pointer',
-    },
-    position: 'absolute',
-    // top: theme.spacing.xs,
-    // left: theme.spacing.xs,
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    width: '100%',
-    height: '100%',
-    backgroundColor: '#f8f9fa',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    textAlign: 'center',
-  },
-
-  title: {
-    '&:hover': {
-      cursor: 'pointer',
-    },
-    fontSize: theme.fontSizes.sm,
-    marginTop: theme.spacing.sm,
-  },
-}));
 
 export interface Props extends Document {
   onClick: (path: string) => void;
 }
 
 export default function AccountDocumentLine(props: Props) {
-  const { classes } = useStyles();
 
   const canPreview = isDocumentAnImage(props.path);
 
   return (
-    <Card shadow="sm" p="xs" radius="sm" withBorder
-          onClick={isDocumentAnImage(props.path) ? () => props.onClick(props.path) : undefined}>
-      <Card.Section className={classes.imgBox}>
+    <div className="flex flex-col border rounded-sm shadow-sm" onClick={isDocumentAnImage(props.path) ? () => props.onClick(props.path) : undefined}>
+      <div 
+        className="relative after:content-[''] after:block after:pb-[75%]"
+      >
         {canPreview ? (
           <img
-            className={classes.img}
+            className="absolute inset-0 w-full h-full object-cover hover:cursor-pointer rounded-t-sm"
             alt={props.filename}
             src={canPreview ? `${serverBaseUrl}/api/documents/${Buffer.from(props.path).toString('base64')}` : ''}
           />
         ) : (
-          <Box className={classes.empty}>This document cannot be previewed</Box>
+          <p className="absolute inset-0 w-full h-full flex items-center justify-center text-center bg-gray-100 text-gray-500">This document cannot be previewed</p>
         )}
-      </Card.Section>
-
-      <Text fw={500} lineClamp={1} className={classes.title}>
+      </div>
+      <div className="text-sm m-2 font-medium truncate hover:cursor-pointer">
         {props.filename}
-      </Text>
-    </Card>
+        {/* todo add tooltips */}
+      </div>
+    </div>
+
   );
 }
