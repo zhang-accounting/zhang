6b9c7930964f632130ff82172dd980374c7bc636
Kilerd Chan <blove694@gmail.com>
2024-10-06 10:36:17 +0800

    feat: dropzone

--- frontend/src/components/AccountDocumentUpload.tsx
+++ frontend/src/components/AccountDocumentUpload.tsx
@@ -1,8 +1,9 @@
-import { Dropzone } from '@mantine/dropzone';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { FileWithPath } from 'react-dropzone';
 import { useSWRConfig } from 'swr';
 import { axiosInstance } from '../global.ts';
+import {useDropzone} from 'react-dropzone'
+import Dropzone from 'react-dropzone'
 
 interface Props {
   url: string;
@@ -11,6 +12,15 @@ interface Props {
 export default function AccountDocumentUpload(props: Props) {
   const [files, setFiles] = useState<FileWithPath[]>([]);
   const { mutate } = useSWRConfig();
+
+
+
+  const onDrop = useCallback((acceptedFiles: FileWithPath[]) => {
+    setFiles(acceptedFiles);
+  }, [])
+
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
   useEffect(() => {
     if (files.length > 0) {
       const formData = new FormData();
@@ -37,20 +47,18 @@ export default function AccountDocumentUpload(props: Props) {
   ));
 
   return (
-    <Dropzone onDrop={setFiles} radius="sm" maxSize={30 * 1024 ** 2}>
-      {filesDom.length > 0 ? (
-        <div>
-          <ul>{filesDom}</ul>
+
+    <div {...getRootProps()} className="relative overflow-hidden rounded-md after:content-[''] after:block after:pb-[100%] bg-gray-100 dark:bg-dark-700">
+      <input {...getInputProps()} />
+      {
+        isDragActive ?
+        <div className="absolute w-full h-full flex flex-col items-center justify-center text-center p-4">
+        <p>Drop the files here ...</p> :
         </div>
-      ) : (
-        <div style={{ pointerEvents: 'none' }}>
-          <div className="text-center font-bold text-lg mt-4">
-            <Dropzone.Accept>Drop files here</Dropzone.Accept>
-            <Dropzone.Reject>Pdf file less than 30mb</Dropzone.Reject>
-            <Dropzone.Idle>Upload Document</Dropzone.Idle>
-          </div>
+        : <div className="absolute w-full h-full flex flex-col items-center justify-center text-center p-4">
+        <p>Drag 'n' drop some files here, or click to select files</p>
         </div>
-      )}
-    </Dropzone>
+      }
+    </div>
   );
 }
