ae7121a1d54e0a624b3ab477a6639e5328a333eb
Kilerd Chan <blove694@gmail.com>
2024-10-19 12:48:27 +0800

    fix: fix the sidebar and scroll the content only

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -77,11 +77,11 @@ export default function App() {
   }, [mutate]);
 
   return (
-    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[220px_1fr]">
+    <div className="grid h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[220px_1fr]">
       <Sidebar />
-      <div className="flex flex-col sm:gap-4 sm:py-4">
+      <div className="flex flex-col sm:gap-4 sm:py-4 overflow-hidden">
         <Nav />
-        <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
+        <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 overflow-scroll">
           <Router />
         </main>
       </div>
